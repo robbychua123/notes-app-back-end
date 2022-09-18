cf7d3c14aebab785d61522fc703cd83832449d78
robby robby <robby.chua@pmberjaya.com>
2022-09-18 21:18:39 +0700

    detailing

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
-
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+// const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
-
-  server.route(routes);
-
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator:NotesValidator
+    },
+  });
+ 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
